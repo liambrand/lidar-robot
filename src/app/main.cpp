8923525dde62f6a6ea70a298af0e7084c0f37270
Liam Brand <liam.brand@northumbria.ac.uk>
2019-02-18 13:18:04 +0000

    Can read and write to SD card

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <ucos_ii.h>
 #include <mbed.h>
+#include <SDFileSystem.h>
 
 /*
 *********************************************************************************************************
@@ -37,6 +38,15 @@ static void appTaskLidar(void *pdata);
 *********************************************************************************************************
 */
 
+#define MOSI		PTE3
+#define MISO		PTE1
+#define	SCLK		PTE2
+#define	CS		PTE4
+#define	DETECT		PTE6
+
+SDFileSystem sd(MOSI, MISO, SCLK, PTE4, "sd");
+char buffer[1024];
+
 Serial pc(USBTX, USBRX);
 Serial device(D1, D0, 115200);
 DigitalOut dtr(D7);
@@ -66,6 +76,24 @@ int main() {
                LIDAR_PRIO);
 
   
+  printf("\nWriting data to the sd card \n");
+   FILE *fp = fopen("/sd/hello.txt", "w");
+   if (fp == NULL) {
+       pc.printf("Unable to write the file \n");
+   } else {
+       fprintf(fp, "mbed SDCard application!");
+       fclose(fp);
+       pc.printf("File successfully written! \n");
+   }
+
+  printf("\nReading data from the SD card. \n");
+   fp = fopen("/sd/hello.txt", "r");
+   if (fp != NULL) {
+       int size = fread(buffer, sizeof(char), 1024, fp);
+       pc.printf("Number of data read: %d, text from hello.txt file: %s \n", size, buffer);
+       fclose(fp);
+   }
+
   /* Start the OS */
   OSStart();                                                  
   
@@ -83,16 +111,18 @@ static void appTaskLidar(void *pdata) {
   
   /* Start the OS ticker -- must be done in the highest priority task */
   SysTick_Config(SystemCoreClock / OS_TICKS_PER_SEC);
-  dtr = 1;
+  //dtr = 1;
 
-	device.puts(startRequest);
+	//device.puts(startRequest);
 
   /* Task main loop */
   while (true) {
-		if(device.readable()) {
-			pc.putc(device.getc());
-		}
+    pc.printf("appTaskLidar Running!\n");
+		//if(device.readable()) {
+			//pc.putc(device.getc());
+	//}
 
-  OSTimeDlyHMSM(0,0,0,10);
+  OSTimeDlyHMSM(0,0,1,0);
   }
 }
+
