fe1c1f525fe24d1c85ba55a9c9e1952d417c5ef8
Liam Brand <liam.brand@northumbria.ac.uk>
2019-03-12 13:15:04 +0000

    Movement demo methods implemented

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -69,6 +69,12 @@ char expressScanRequest[] = {0xA5, 0x82, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x2
 char resetRequest[] = {0xA5, 0x40};
 char stopRequest[] = {0xA5, 0x25};
 
+/* TEMP HACK FOR MOVEMENT DEMONSTRATION */
+static void goForward(void);
+static void goBackward(void);
+static void goLeft(void);
+static void goRight(void);
+
 
 /*
 *********************************************************************************************************
@@ -86,28 +92,6 @@ int main() {
                (void *)0,
                (OS_STK *)&lidarStk[LIDAR_STK_SIZE - 1],
                LIDAR_PRIO);
-
-
-	
-  
-  	printf("\nWriting data to the sd card \n");
-   	FILE *fp = fopen("/sd/hello.bin", "wb"); // w for write, b for binary
-  	 if (fp == NULL) {
-       pc.printf("Unable to write the file \n");
-   	} 	else {
-				//fprintf(fp, "00101010 00101011 11010010 11101010 00101010");
-       	fprintf(fp, "00101010 00101011 11010010 11101010 00101010");
-       	fclose(fp);
-       	pc.printf("File successfully written! \n");
-   	}
-
-  	printf("\nReading data from the SD card. \n");
-   	fp = fopen("/sd/hello.bin", "rb"); // r for read, b for binary
-   	if (fp != NULL) {
-       	int size = fread(buffer, sizeof(char), 1024, fp);
-       	pc.printf("Number of data read: %d, text from hello.bin file: %s \n", size, buffer);
-       	fclose(fp);
-   	}
 
   /* Start the OS */
   OSStart();                                                  
@@ -127,24 +111,65 @@ static void appTaskLidar(void *pdata) {
   /* Start the OS ticker -- must be done in the highest priority task */
   SysTick_Config(SystemCoreClock / OS_TICKS_PER_SEC);
   //dtr = 1;
-  M1_DIR = 0;
-  M2_DIR = 1;
-  M3_DIR = 0;
-
-  M1_SPD = 0.0f;
-  M2_SPD = 0.0f;
-  M3_SPD = 0;
-
-	//device.puts(startRequest);
+	
 
   /* Task main loop */
   while (true) {
-    //pc.printf("appTaskLidar Running!\n");
-		//if(device.readable()) {
-			//pc.putc(device.getc());
-	//}
+    goForward();
+    OSTimeDlyHMSM(0,0,1,0);
+    goBackward();
+    OSTimeDlyHMSM(0,0,1,0);
+    goLeft();
+    OSTimeDlyHMSM(0,0,1,0);
+    goRight();
+    OSTimeDlyHMSM(0,0,1,0);
 
-  OSTimeDlyHMSM(0,0,1,0);
+    //pc.printf("hello!");
+    //OSTimeDlyHMSM(0,0,1,0);
   }
+}
+
+static void goForward() {
+  pc.printf("goForward");
+  M1_DIR = 1;
+  M2_DIR = 1;
+  M3_DIR = 1;
+
+  M1_SPD = 0;
+  M2_SPD = 0.5f;
+  M3_SPD = 0.5f;
+}
+
+static void goBackward() {
+  pc.printf("goBackward");
+  M1_DIR = 1;
+  M2_DIR = 0;
+  M3_DIR = 0;
+
+  M1_SPD = 0;
+  M2_SPD = 0.5f;
+  M3_SPD = 0.5f;
+}
+
+static void goLeft() {
+  pc.printf("goLeft");
+  M1_DIR = 0;
+  M2_DIR = 0;
+  M3_DIR = 1;
+
+  M1_SPD = 0.5f;
+  M2_SPD = 0.25f;
+  M3_SPD = 0.25f;
+}
+
+static void goRight() {
+  pc.printf("goRight");
+  M1_DIR = 1;
+  M2_DIR = 1;
+  M3_DIR = 0;
+
+  M1_SPD = 0.5f;
+  M2_SPD = 0.25f;
+  M3_SPD = 0.25f;
 }
 
