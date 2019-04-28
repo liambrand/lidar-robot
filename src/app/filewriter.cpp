a01877cc9564f0a08a5df5647447372d64fb9338
Student <student@C17776079.offcampusnetwork.co.uk>
2019-04-28 11:00:25 +0100

    Filled in write readings

--- src/app/filewriter.cpp
+++ src/app/filewriter.cpp
@@ -10,7 +10,23 @@ FileWriter::FileWriter() {
 	
 }
 
-void writeReadings(float angles[], float measurements[]) {
-  // method stub
+void writeReadings(float angles[], float distances[]) {
+  int arraySize = sizeof(readingsBuffer)/sizeof(float);
+  
+	// Create the readings file on the Micro SD-Card
+	FILE *fp = fopen("/sd/readings.txt", "w");
+
+	// Error checking
+  if (fp == NULL) {
+    pc.printf("Unable to access/create file \n");
+  }
+
+	// Iterate through the readingsBuffer and write the angle/distance pairs
+	for(int i = 0; i < arraySize; i++) {
+		fprintf(fp, "%f %f", angles[i], distances[i]);
+	}
+
+	// Close file
+fclose(fp);
 }
 
